9385dcebfb999b55ab3a09860d6eb63467e4da00
Florent Messa <florent.messa@gmail.com>
2013-11-06 16:45:57 +0100

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
- 
+
+
 setup(
     name='django-email-confirmation',
-    version='0.1.3',
+    version='0.2.1',
     description='Simple email confirmation for the Django web framework.',
     long_description=open('docs/index.txt').read(),
     author='James Tauber',
     author_email='jtauber@jtauber.com',
     url='http://code.google.com/p/django-email-confirmation/',
     packages=find_packages(exclude=['devproject.devtest', 'devproject']),
-    package_data = {
+    package_data={
         'emailconfirmation': [
             'templates/emailconfirmation/*.txt'
         ],
